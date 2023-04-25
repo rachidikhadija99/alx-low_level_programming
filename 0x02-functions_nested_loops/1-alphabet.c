f171bc55be3806792ad16f92945f7409085c3d87
khadija1 <rachidikhadija99@gmail.com>
2023-04-25 13:23:29 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,9 @@
  */
 void print_alphabet(void)
 {
-     char letter;
+	char i;
 
-     for (letter ='a'; letter <='z';letter++)
-     {
-       _putchar(letter);
-     }
-     _putchar('\n');
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
 }
