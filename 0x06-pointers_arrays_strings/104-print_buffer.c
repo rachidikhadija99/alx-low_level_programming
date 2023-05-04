301fa696f97178058938d08efd3ff82e3c3776e0
khadija1 <rachidikhadija99@gmail.com>
2023-05-04 15:47:24 -0700

    task 104

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -20,76 +20,69 @@ void print_buffer(char *b, int size)
 
 {
 
-        int o, j, i;
+	int o, j, i;
 
 
-        o = 0;
+	o = 0;
 
 
-        if (size <= 0)
+	if (size <= 0)
 
-        {
+	{
 
-                printf("\n");
+	printf("\n");
+	return;
 
-                return;
+	}
 
-        }
+	while (o < size)
 
-        while (o < size)
+	{
 
-        {
+	j = size - o < 10 ? size - o : 10;
 
-                j = size - o < 10 ? size - o : 10;
+	printf("%08x: ", o);
 
-                printf("%08x: ", o);
+	for (i = 0; i < 10; i++)
 
-                for (i = 0; i < 10; i++)
+	{
 
-                {
+	if (i < j)
 
-                        if (i < j)
+	printf("%02x", *(b + o + i));
 
-                                printf("%02x", *(b + o + i));
+	else
 
-                        else
+	printf("  ");
 
-                                printf("  ");
+	if (i % 2)
 
-                        if (i % 2)
+	{
 
-                        {
+	printf(" ");
 
-                                printf(" ");
+	}
+	}
+	for (i = 0; i < j; i++)
 
-                        }
+	{
 
-                }
-
-                for (i = 0; i < j; i++)
-
-                {
-
-                        int c = *(b + o + i);
+	int c = *(b + o + i);
 
 
-                        if (c < 32 || c > 132)
+	if (c < 32 || c > 132)
 
-                        {
+	{
+	c = '.';
+	printf("%c", c);
 
-                                c = '.';
+	}
 
-                        }
+	printf("\n");
 
-                        printf("%c", c);
+	o += 10;
 
-                }
-
-                printf("\n");
-
-                o += 10;
-
-        }
+	}
 
 }
 
